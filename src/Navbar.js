845e12f52ab9959e967d01b74d1417230d13cf3f
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-12-13 22:06:19 +0700

    linked context for darkMode

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,22 +6,26 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import Switch from '@material-ui/core/Switch';
+
 import { withStyles } from '@material-ui/core/styles';
+import { ThemeContext } from './contexts/ThemeContext';
+
 import styles from './styles/NavBarStyles';
 
 function Navbar(props) {
+	const { isDarkmode, toggleIsDarkmode } = useContext(ThemeContext);
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<AppBar position='static' color='primary'>
+			<AppBar position='static' color={isDarkmode ? 'default' : 'primary'}>
 				<Toolbar>
 					<IconButton className={classes.menuButton} color='inherit'>
-						<span>🇫🇷</span>
+						<span role='img' aria-label="french flag">🇫🇷</span>
 					</IconButton>
 					<Typography className={classes.title} variant='h6' color='inherit'>
 						App Title
 					</Typography>
-					<Switch />
+					<Switch onClick={toggleIsDarkmode} />
 					<div className={classes.grow} />
 					<div className={classes.search}>
 						<div className={classes.searchIcon}>
