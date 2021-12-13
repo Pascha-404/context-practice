a4885a955e46af2ee9d8d51662cd4e05220545be
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-12-13 22:40:59 +0700

    added contextComp for language

---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+function LanguageProvider(props) {
+	const [language, setLanguage] = useState('english');
+
+	const changeLanguage = e => setLanguage(e.target.value);
+
+	return (
+		<LanguageContext.Provider value={{ language, changeLanguage }}>
+			{props.children}
+		</LanguageContext.Provider>
+	);
+}
+
+export { LanguageContext, LanguageProvider };
